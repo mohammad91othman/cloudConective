af161fa1f239ebc1b3a6fd4dad400f7e91712059
“Mohammad <othman@lamsaworld.com>
2017-08-08 22:52:57 +0300

    first commit

---
+++ othmanTask/WSHandler.swift
@@ -0,0 +1,13 @@
+//
+//  WSHandler.swift
+//  othmanTask
+//
+//  Created by Mohammad Othman on 8/4/17.
+//  Copyright © 2017 Mohammad Othman. All rights reserved.
+//
+
+import UIKit
+
+class WSHandler: NSObject {
+
+}
---
+++ othmanTask/cellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  cellTableViewCell.swift
+//  othmanTask
+//
+//  Created by Mohammad Othman on 8/7/17.
+//  Copyright © 2017 Mohammad Othman. All rights reserved.
+//
+
+import UIKit
+
+class cellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
